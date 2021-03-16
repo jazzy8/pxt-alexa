f408d14cad37940899aae69361585904d691e974
jazzy8 <65190962+jazzy8@users.noreply.github.com>
2021-03-15 21:00:40 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,15 +3,13 @@ input.onSound(DetectedSound.Loud, function () {
     strip.showColor(neopixel.rgb(0, 213, 255))
     range = strip.range(3, 6)
     range.showColor(neopixel.colors(NeoPixelColors.Blue))
+    strip.show()
     basic.pause(10000)
-    pins.digitalWritePin(DigitalPin.P8, 0)
-    basic.pause(100)
-    pins.digitalWritePin(DigitalPin.P8, 1)
+    strip.show()
 })
 let range: neopixel.Strip = null
 let minks = 0
 let strip: neopixel.Strip = null
 strip = neopixel.create(DigitalPin.P1, 12, NeoPixelMode.RGB)
-pins.digitalWritePin(DigitalPin.P8, 0)
-basic.pause(100)
-pins.digitalWritePin(DigitalPin.P8, 1)
+strip.clear()
+strip.show()
